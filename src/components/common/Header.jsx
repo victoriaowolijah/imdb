6d86e5ca4ef6d818053eaa26080c82da20d98155
victoria owolijah <victoriaowolijah@gmail.com>
2023-05-09 20:29:38 +0100

    new

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { AppBar, Toolbar, styled } from '@mui/material'
+
+const StyledToolBar = styled(Toolbar)`
+    background: #121212;
+    min-height:56px;
+
+`
+
+const Header = () => {
+  return (
+    
+    <AppBar>
+        <Toolbar>
+
+        </Toolbar>
+    </AppBar>
+   
+  )
+}
+
+export default Header
